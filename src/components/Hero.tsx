7d946c1288f12e34e332eaf88a43df351d807021
bzsol <bereczzsolt2000@gmail.com>
2023-09-12 18:51:27 +0200

    Added Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,16 @@
+import logo from '../assets/logo.png'
+
 function Hero() {
   return (
-    <div className="bg-green-500 py-20">
-        
+    <div className="bg-green-400 py-20">
+      <div className='flex sm:flex-row flex-col sm:justify-center'>
+      <img className='mx-20' src={logo} height={250} width={250}></img>
+      <div className='flex flex-col text-green-800 text-4xl p-10 font-bold mx-14'>
+        <h1 className='py-1'>ECOLOGY</h1>
+        <h1 className='py-1'>DEMOCRACY</h1>
+        <h1 className='py-1'>CAMPAIGN</h1>
+      </div>
+      </div>
     </div>
   )
 }
