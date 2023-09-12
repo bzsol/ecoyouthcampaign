8779802eac42025e41a1143e72f0a6ba0857ae77
bzsol <bereczzsolt2000@gmail.com>
2023-09-12 17:52:19 +0200

    Added components / Hero / Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Hero></Hero>
+      <Navbar></Navbar>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+
   )
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,9 @@
+function Hero() {
+  return (
+    <div className="bg-green-500 py-20">
+        
+    </div>
+  )
+}
+
+export default Hero
