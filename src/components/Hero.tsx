b0515cb6649809ed749621e70a6b3ec4c80aa664
bzsol <bereczzsolt2000@gmail.com>
2023-09-17 13:08:38 +0200

    deleted assets

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,10 @@
-import logo from '../assets/logo.png'
+
 
 function Hero() {
   return (
     <div className="bg-green-400 py-20">
       <div className='flex flex-col justify-start md:justify-center md:flex-row '>
-      <img className='mx-auto md:mx-0' src={logo} height={250} width={250}></img>
+      <img className='mx-auto md:mx-0' src="/logo.png" height={250} width={250}></img>
       <div className='flex flex-col text-textcolor text-4xl p-10 font-bold mx-auto md:mx-0'>
         <h1 className='py-1'>ECOLOGY</h1>
         <h1 className='py-1'>DEMOCRACY</h1>
