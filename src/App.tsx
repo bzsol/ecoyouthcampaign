cf3c45882f1ca16616131044acf6daee80769e71
bzsol <bereczzsolt2000@gmail.com>
2023-10-07 23:05:36 +0200

    edited meta tags

--- src/App.tsx
+++ src/App.tsx
@@ -9,21 +9,12 @@ import SecondWorkshop from "./Pages/SecondWorkshop";
 import ThirdWorkshop from "./Pages/ThirdWorkshop";
 // import i18n (needs to be bundled ;))
 import './i18n';
-import { Helmet } from 'react-helmet';
 
 
 
 export default function App() {
   return (
       <div>
-      <Helmet>
-        <title>Eco Youth Campaign</title>
-        <meta name="description" content="Programunkban szeretnénk elérni, hogy 3 város fiataljai összefogjanak és egy olyan online és offline kampányt indítsanak el a környezetvédelem és egyúttal demokratikus helyi közélet érdekében, mely alapján alapvető szemléletváltást tudunk elérni." />
-        <meta name="theme-color" content="#008f68" />
-        <meta name="robots" content="noimageindex"/>
-        <meta charSet="utf-8" />
-        <meta name="keywords"content="Eco Youth Campaign,ecoyouthcampaign,nyíregyháza,eco youth campaign"></meta>
-      </Helmet>
       <Router>
       <Routes>
         <Route path="/" element={<Home/>} />
