3e6dbc8005e763045f23bd0cf73626b8b896d5ab
peimlidorka <pmldrk@gmail.com>
2023-10-29 16:32:43 +0100

    updated sixth fifth texts

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -34,11 +34,26 @@ function Home() {
         { src: '/fourth/3.jpg', alt: '3' },
         { src: '/fourth/4.jpg', alt: '4' },
     ]
+    const fifth = [
+        { src: '/fifth/1.jpg', alt: '1' },
+        { src: '/fifth/2.jpg', alt: '2' },
+        { src: '/fifth/3.jpg', alt: '3' },
+        { src: '/fifth/4.jpg', alt: '4' },
+    ]
+    const sixth = [
+        { src: '/sixth/1.jpg', alt: '1' },
+        { src: '/sixth/2.jpg', alt: '2' },
+        { src: '/sixth/3.jpg', alt: '3' },
+        { src: '/sixth/4.jpg', alt: '4' },
+    ]
+
     return (
         <div id="main" className="App">
             <div className="flex flex-col">
                 <Hero></Hero>
                 <Navbar></Navbar>
+                <Section url="/sixth-workshop" date={t('workshop.date_sixth')} title={t('workshop.sixth')} text={t('workshop.sixth_text')} photo={sixth}></Section>
+                <Section url="/fifth-workshop" date={t('workshop.date_fifth')} title={t('workshop.fifth')} text={t('workshop.fifth_text')} photo={fifth}></Section>
                 <Section url="/fourth-workshop" date={t('workshop.date_fourth')} title={t('workshop.fourth')} text={t('workshop.fourth_text')} photo={fourth}></Section>
                 <Section url="/third-workshop" date={t('workshop.date_third')} title={t('workshop.third')} text={t('workshop.third_text')} photo={third}></Section>
                 <Section url="/second-workshop" date={t('workshop.date_second')}  title={t('workshop.second')} text={t('workshop.second_text')}  photo={second}></Section>
